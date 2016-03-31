01b11855a2c901889934fd076703e205108ab911
Jaap Versteegh <j.r.versteegh@gmail.com>
2016-03-31 16:09:35 +0200

    - Attempt to templatize chips

--- src/chips.cc
+++ src/chips.cc
@@ -34,180 +34,24 @@ const int itg3200_address = 0x68;
 const int itg3205_address = 0x68; // alternative 0x69: pin 9 high
 const int bmp085_address = 0x77;
 
-void HMC5843<>::initialize()
+
+template <class Device>
+void BMP085<Device>::set_calibration_data(const Words& calibration_data)
 {
-  // 20Hz output, no bias
-  device().write_byte(0x00, 0x14);
-  // 1 Gauss range
-  device().write_byte(0x01, 0x20);
-  // Continuous data aquisition
-  device().write_byte(0x02, 0x00);
+  ac1_ = static_cast<int16_t>(calibration_data[0]);
+  ac2_ = static_cast<int16_t>(calibration_data[1]);
+  ac3_ = static_cast<int16_t>(calibration_data[2]);
+  ac4_ = static_cast<uint16_t>(calibration_data[3]);
+  ac5_ = static_cast<uint16_t>(calibration_data[4]);
+  ac6_ = static_cast<uint16_t>(calibration_data[5]);
+  b1_ = static_cast<int16_t>(calibration_data[6]);
+  b2_ = static_cast<int16_t>(calibration_data[7]);
+  mb_ = static_cast<int16_t>(calibration_data[8]);
+  mc_ = static_cast<int16_t>(calibration_data[9]);
+  md_ = static_cast<int16_t>(calibration_data[10]);
 }
 
-void HMC5843::poll()
-{
-  //Byte ready = device().read_byte(0x09) & 0x01;
-  //if (ready) {
-  Words words = device().read_words(0x03, 3);
-  Sample_t sample = Sample_t(Vector_t(
-      static_cast<Value_t>(static_cast<int16_t>(words[1])) * comp_x_fact + comp_x_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[0])) * comp_y_fact + comp_y_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[2])) * comp_z_fact + comp_z_offs
-  ));
-  push_sample(sample);
-  //}
-}
-
-void HMC5843<Device>::finalize()
-{
-  // Put device to sleep
-  device().write_byte(0x02, 0x03);
-}
-
-void ADXL345<Device>::initialize()
-{
-  // Clear the sleep bit (when it was set)
-  device().write_byte(0x2D, 0x00);
-  // Enable measure bit (get out of standby)
-  device().write_byte(0x2D, 0x08);
-  // Set FULL_RES bit for full resolution on all g scales
-  // and the maximum g scale -> +-16g (why would you want to reduce this??)
-  // scale is 4mg/bit
-  device().write_byte(0x31, 0x0B);
-}
-
-void ADXL345<Device>::poll()
-{
-  Words words = device().read_words(0x32, 3);
-  Sample_t sample = Sample_t(Vector_t(
-      static_cast<Value_t>(static_cast<int16_t>(words[0])) * accel_x_fact + accel_x_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[1])) * accel_y_fact + accel_y_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[2])) * accel_z_fact + accel_z_offs 
-  ));
-  push_sample(sample);
-}
-
-void ADXL345<Device>::finalize()
-{
-  // Disable measure bit (set to standby)
-  device().write_byte(0x2D, 0x00);
-  // Put the device to sleep
-  device().write_byte(0x2D, 0x07);
-}
-
-void BMA180<Device>::initialize()
-{
-  // Get chip information
-  set_chip_id(device().read_byte(0x00));
-  set_chip_version(device().read_byte(0x01));
-
-  // Disable wake up mode (bit 0): never sleep
-  device().write_byte(0x0D, 0x01);
-  // Put in ultra low noise mode
-  device().write_byte(0x30, 0x01);
-  // Set range to -2/+2g: 0.25mg/bit
-  device().write_byte(0x35, 0x04);
-  // set filter range to 20Hz (defaults to 1200Hz)
-  device().write_byte(0x20, 0x10);
-}
-
-void BMA180<Device>::poll()
-{
-  Words xyz = device().read_words(0x02, 3);
-  Byte temp = device().read_byte(0x08);
-  Sample_t sample = Sample_t(Vector_t(
-      static_cast<Value_t>(static_cast<int16_t>(xyz[0] >> 2)) * accel_x_fact + accel_x_offs,
-      static_cast<Value_t>(static_cast<int16_t>(xyz[1] >> 2)) * accel_y_fact + accel_y_offs,
-      static_cast<Value_t>(static_cast<int16_t>(xyz[2] >> 2)) * accel_z_fact + accel_z_offs 
-  ), static_cast<Value_t>(static_cast<int8_t>(temp)) * gyro_temp_fact + gyro_temp_offs);
-  push_sample(sample);
-}
-
-void BMA180<Device>::finalize()
-{
-  // Put the device to sleep
-  device().write_byte(0x0D, 0x02);
-}
-
-
-void ITG3200<Device>::initialize()
-{
-  // First reset the chip
-  device().write_byte(0x3E, 0x80);
-  // Wait a little for it to come back up
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  // Sample at 20Hz: 1kHz / 50 (49 + 1)
-  device().write_byte(0x15, 0x31);
-  // Select range: 0x03 << 3 and low pass filter of 10Hz: 0x05
-  device().write_byte(0x16, 0x1D);
-  // Get out of sleep and select PLL with X Gyro reference as clock
-  device().write_byte(0x3E, 0x01);
-}
-
-void ITG3200<Device>::poll()
-{
-  Words words = device().read_words(0x1B, 4);
-  Sample_t sample = Sample_t(Vector_t(
-      static_cast<Value_t>(static_cast<int16_t>(words[1])) * gyro_x_fact + gyro_x_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[2])) * gyro_y_fact + gyro_y_offs,
-      static_cast<Value_t>(static_cast<int16_t>(words[3])) * gyro_z_fact + gyro_z_offs
-    ), static_cast<Value_t>(static_cast<int16_t>(words[0])) * gyro_temp_fact + gyro_temp_offs
-  ); 
-  push_sample(sample);
-}
-
-void ITG3200<Device>::finalize()
-{
-  // Put to sleep and select internal oscillator as clock
-  device().write_byte(0x3E, 0x40);
-}
-
-
-void BMP085<Device>::initialize()
-{
-  // Read calibration data from EEPROM
-  Words words = device().read_words(0xAA, 11);
-  ac1_ = static_cast<int16_t>(words[0]);
-  ac2_ = static_cast<int16_t>(words[1]);
-  ac3_ = static_cast<int16_t>(words[2]);
-  ac4_ = static_cast<uint16_t>(words[3]);
-  ac5_ = static_cast<uint16_t>(words[4]);
-  ac6_ = static_cast<uint16_t>(words[5]);
-  b1_ = static_cast<int16_t>(words[6]);
-  b2_ = static_cast<int16_t>(words[7]);
-  mb_ = static_cast<int16_t>(words[8]);
-  mc_ = static_cast<int16_t>(words[9]);
-  md_ = static_cast<int16_t>(words[10]);
-}
-
-void BMP085<Device>::poll()
-{
-  if (loop_count_ % 120 == 0) {
-    loop_count_ = 0;
-    // Get temperature
-    device().write_byte(0xF4, 0x2E);
-  } else if (loop_count_ == 1) {
-    // Read temperature
-    Byte raw_temp = device().read_byte(0xF6);
-    raw_temp = eval_temp(raw_temp);
-  } else if (loop_count_ % 2 == 0) {
-    // Read pressure 8 times oversampling: takes 25ms
-    device().write_byte(0xF4, 0xF4);
-  } else {
-    Word raw_pressure = device().read_word(0xF6);
-    int32_t pressure = eval_pressure(raw_pressure);
-    Sample_t sample = Sample_t(Vector_t(
-        static_cast<Value_t>(pressure) * pressure_fact + pressure_offs, 0, 0), 
-        static_cast<Value_t>(temp_) * temp_fact + temp_offs); 
-    push_sample(sample);
-  }
-  loop_count_++;
-}
-
-void BMP085<Device>::finalize()
-{
-}
-
+template <class Device>
 int32_t BMP085<Device>::eval_temp(const Word raw_temp)
 {
   int32_t result = static_cast<int32_t>(raw_temp);
@@ -218,6 +62,7 @@ int32_t BMP085<Device>::eval_temp(const Word raw_temp)
   return result;
 }
 
+template <class Device>
 int32_t BMP085<Device>::eval_pressure(const int32_t raw_pressure)
 {
   int32_t result = 0;
