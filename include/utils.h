a660e077589a02ff82e8a49a81a397f31efea760
Jaap Versteegh <j.r.versteegh@gmail.com>
2018-02-23 11:37:13 +0100

    Split off some inline general functions into utils.h

---
+++ include/utils.h
@@ -0,0 +1,51 @@
+/* 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+/** \file
+ * \author Jaap Versteegh <j.r.versteegh@gmail.com>
+ * Declares some utility functions
+ */
+#ifndef MRU_UTILS_H
+#define MRU_UTILS_H
+
+
+namespace mru {
+
+template<typename FT> 
+inline
+typename std::enable_if<std::is_floating_point<FT>::value, FT>::type 
+d2r(const FT degs) {
+  static const FT rpd = 0.017453292519943295769236907684886;
+  return rpd * degs;
+}
+
+template<typename FT> 
+inline 
+typename std::enable_if<std::is_floating_point<FT>::value, FT>::type 
+r2d(const FT rads) {
+  static const FT dpr = 57.295779513082320876798154814105;
+  return dpr * rads;
+}
+
+template<typename FT> 
+inline 
+typename std::enable_if<std::is_floating_point<FT>::value, FT>::type 
+sqr(const FT value) {
+  return value * value;
+}
+
+}  // namespace mru 
+
+#endif
